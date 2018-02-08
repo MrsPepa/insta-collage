b1405457609426ad9e04ad7fca45bf5755f62eec
MrsPepa <jose.leon1996@gmail.com>
2018-02-08 09:28:45 -0300

    Drag and Drop finalizado, empezando edicion hacker

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+//Iniciando collage y acceso de imagenes
+function drag(event) {
+  console.log(event.target.src);
+  event.dataTransfer.setData('text', event.target.id);//tipo de formato a transferir
+}
+
+function admitirDrop(event) {
+  event.preventDefault();
+}
+
+function drop(event) {
+  event.preventDefault();
+  var id_foto = event.dataTransfer.getData('text');
+  event.target.appendChild(document.getElementById(id_foto));
+}
